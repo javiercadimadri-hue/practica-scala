d1dbc98ffcef4bfd46a5f24e7dc533e6a593dcf4
Javier Abadia Alfaro <javiercadimadri@gmail.com>
2026-09-16 12:36:36 +0200

    Completada Parte 1 de la practica

---
+++ Practica 1/Entorno3/scala-intellij/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val entorno = "IntelliJ IDEA"
+
+    println("Práctica de programación básica con Scala")
+    println(s"Ejecutando desde:$entorno")
+  }
+}
